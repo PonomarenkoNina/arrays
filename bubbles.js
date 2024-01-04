8f423d222f5e8d07c754b1d853bed516182c22db
PonomarenkoNina <ninulka@ukr.net>
2024-01-04 18:56:34 +0200

    added parallel arrays, determine the most cost-effective sample (with minimal costs)

--- bubbles.js
+++ bubbles.js
@@ -129,3 +129,27 @@ console.log("Highest bubble score: " + highScore);
 
 var bestSolutions = getBestResults(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
+
+// Parallel arrays, determine the most cost-effective sample (with minimal costs)
+var costs = [
+  0.25, 0.27, 0.25, 0.25, 0.25, 0.25, 0.33, 0.31, 0.25, 0.29, 0.27, 0.22, 0.31,
+  0.25, 0.25, 0.33, 0.21, 0.25, 0.25, 0.25, 0.28, 0.25, 0.24, 0.22, 0.2, 0.25,
+  0.3, 0.25, 0.24, 0.25, 0.25, 0.25, 0.27, 0.25, 0.26, 0.29,
+];
+function getMostCostEffectiveSolution(scores, costs, highScore) {
+  var cost = 100;
+  var index;
+  for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+      if (cost > i) {
+        index = i;
+        cost = costs[i];
+      }
+    }
+  }
+  return index;
+}
+var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log(
+  "Bubble Solution #" + mostCostEffective + " is the most cost effective"
+);
